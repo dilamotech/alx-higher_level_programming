73b6890a07767e494122b4a23b767c1db1eda317
unknown <ef.dilamo@gmail.com>
2023-03-31 06:43:33 -0700

    changes

--- 0x10-python-network_0/2-delete.sh
+++ 0x10-python-network_0/2-delete.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Sends a DELETE request to URL and displays body of response
+#DELETE request to the URL passed as the first argument and displays the body of the response
 curl -s "$1" -X DELETE
